32e1a649ba49ec8e924ddd657b74f46f56e866bb
dmitrylobanov <dmitry.lobanov@onefactor.com>
2020-08-15 12:10:01 +0300

    #2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,11 @@
 import sbt._
 
 object Dependencies {
+
+  lazy val `scala-logging` = Seq(
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
+    "ch.qos.logback" % "logback-classic" % "1.2.3"
+  )
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
 }
